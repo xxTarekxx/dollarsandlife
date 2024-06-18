03713e8f6f56259f7c2dce05d537c865f3e7fd91
xxTarekxx <tarektawfik90@outlook.com>
2024-06-18 15:29:44 -0500

    blogpost component

---
+++ src/components/BlogPost.tsx
@@ -0,0 +1,94 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+// Styled components
+const Card = styled.div`
+	background: #7f7fd5;
+	border-radius: 10px;
+	box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+	transition: 0.3s;
+	width: 300px;
+	margin: 1rem;
+	text-align: center;
+`;
+
+const CardHeader = styled.div`
+	img {
+		width: 100%;
+		border-radius: 10px 10px 0 0;
+	}
+`;
+
+const CardBody = styled.div`
+	padding: 2px 16px;
+`;
+
+const CardFooter = styled.div`
+	padding: 2px 16px;
+`;
+
+const UserImage = styled.img`
+	border-radius: 50%;
+	width: 40px;
+`;
+
+const UserInfo = styled.div`
+	display: inline-block;
+	vertical-align: middle;
+	margin-left: 8px;
+`;
+
+const Tag = styled.span`
+	display: inline-block;
+	background-color: #d1913c;
+	color: white;
+	border-radius: 10px;
+	padding: 5px;
+	margin-right: 5px;
+	font-size: 12px;
+`;
+
+// BlogPost component props interface
+interface BlogPostProps {
+	id: number;
+	title: string;
+	imageUrl: string;
+	content: string;
+	author: string;
+	datePosted: string;
+}
+
+// BlogPost component
+const BlogPost: React.FC<BlogPostProps> = ({
+	id,
+	title,
+	imageUrl,
+	content,
+	author,
+	datePosted,
+}) => {
+	return (
+		<Link to={`/blog/${id}`} style={{ textDecoration: "none" }}>
+			<Card>
+				<CardHeader>
+					<img src={imageUrl} alt={title} />
+				</CardHeader>
+				<CardBody>
+					<Tag>Category</Tag>
+					<h4>{title}</h4>
+					<p>{content}</p>
+				</CardBody>
+				<CardFooter>
+					<UserImage src='./user-image-placeholder.png' alt='author' />
+					<UserInfo>
+						<h5>{author}</h5>
+						<small>{datePosted}</small>
+					</UserInfo>
+				</CardFooter>
+			</Card>
+		</Link>
+	);
+};
+
+export default BlogPost;
