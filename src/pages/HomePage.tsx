f7545a6c252c20aabf79cece8d285b298d687b03
xxTarekxx <tarektawfik90@outlook.com>
2024-06-19 16:41:07 -0500

    HomePage accessibility

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,9 +5,6 @@ import ManageIcon from "../images/icons/manage-your-finances-icon.png";
 import PassiveIcon from "../images/icons/passive-income.png";
 import DealsIcon from "../images/icons/deal-and-savings-icon.png";
 
-// Define GlobalStyle in the root of your application
-// const GlobalStyle = createGlobalStyle`...`;
-
 const LinksContainer = styled.div`
 	width: 100%;
 	margin: 9% auto;
@@ -21,24 +18,29 @@ const spinAnimation = keyframes`
   100% { transform: rotate(360deg); }
 `;
 
-const LinkBox = styled.div`
+const LinkBox = styled(Link)`
 	display: flex;
 	flex-direction: column;
 	align-items: center;
 	justify-content: center;
-	cursor: pointer;
 	background: white;
 	font-size: 22px;
-	border-radius: 10px;
+	// border-radius: 10px;
 	margin: 1%;
-	text-decoration: none;
 	color: black;
 	box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
 		rgba(0, 0, 0, 0.3) 0px 30px 60px -30px,
 		rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;
 
+	text-align: center;
+	text-decoration: none;
+
 	&:hover img {
 		animation: ${spinAnimation} 0.2s linear 1;
+	}
+
+	&:focus {
+		outline: 3px solid #7600ff; /* Focus state for keyboard navigation */
 	}
 
 	@media (max-width: 768px) {
@@ -52,55 +54,29 @@ const LinkBox = styled.div`
 	}
 `;
 
-const Figure = styled.figure`
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	justify-content: center;
-	text-align: center;
-	margin: 0;
-	padding: 0.5rem;
+const Figcaption = styled.figcaption`
+	color: black;
+	transition: color 0.3s ease-in-out;
 
-	figcaption {
-		color: black;
-		transition: color 0.3s ease-in-out;
-	}
-
-	&:hover figcaption {
+	&:hover {
 		color: #7600ff;
-	}
-
-	a {
-		text-decoration: none;
 	}
 `;
 
 const HomePage = () => {
 	return (
 		<LinksContainer aria-label='Main navigation links'>
-			<LinkBox>
-				<Figure>
-					<Link to='/category/extra-income'>
-						<img src={ManageIcon} alt='Manage Finance Photo' loading='lazy' />
-						<figcaption>Extra Income</figcaption>
-					</Link>
-				</Figure>
+			<LinkBox to='/category/extra-income/' aria-label='Extra Income'>
+				<img src={ManageIcon} alt='Manage Finance Photo' loading='lazy' />
+				<Figcaption>Extra Income</Figcaption>
 			</LinkBox>
-			<LinkBox>
-				<Figure>
-					<Link to='/passive-income'>
-						<img src={PassiveIcon} alt='Passive Income Icon' loading='lazy' />
-						<figcaption>Passive Income</figcaption>
-					</Link>
-				</Figure>
+			<LinkBox to='/passive-income' aria-label='Passive Income'>
+				<img src={PassiveIcon} alt='Passive Income Icon' loading='lazy' />
+				<Figcaption>Passive Income</Figcaption>
 			</LinkBox>
-			<LinkBox>
-				<Figure>
-					<Link to='/amazon-products'>
-						<img src={DealsIcon} alt='Deals And Saving Icon' loading='lazy' />
-						<figcaption>Deals</figcaption>
-					</Link>
-				</Figure>
+			<LinkBox to='/amazon-products' aria-label='Deals'>
+				<img src={DealsIcon} alt='Deals And Saving Icon' loading='lazy' />
+				<Figcaption>Deals</Figcaption>
 			</LinkBox>
 		</LinksContainer>
 	);
