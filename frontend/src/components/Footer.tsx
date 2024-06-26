8b4cf0b45d26c768654617f0419b2cc4b87bc85a
xxTarekxx <tarektawfik90@outlook.com>
2024-06-25 20:50:20 -0500

    refactoring code

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -2,25 +2,20 @@ import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
 import {
 	faYoutube,
 	faFacebook,
 	faInstagram,
 } from "@fortawesome/free-brands-svg-icons";
 
-// Styled components for SocialFlaw
-const SocialContainer = styled.div`
-	// width: 100%;
-	background-color: rgba(255, 255, 255, 0.5);
-	padding: 15px 0px;
+const Container = styled.div`
+	background-color: rgba(255, 255, 255, 0.9);
+	padding: 2rem 1rem;
+	box-sizing: border-box;
 	display: flex;
+	flex-direction: column;
+	align-items: center;
 	justify-content: center;
-	flex-wrap: wrap;
-
-	@media (max-width: 768px) {
-		padding: 25px 0;
-	}
 `;
 
 const SocialLink = styled.a`
@@ -33,55 +28,10 @@ const SocialLink = styled.a`
 	}
 `;
 
-const Youtube = styled(SocialLink)`
-	color: #eb3223;
-`;
-
-const Facebook = styled(SocialLink)`
-	color: #4968ad;
-`;
-
-const Twitter = styled(SocialLink)`
-	color: #49a1eb;
-`;
-
-const Instagram = styled(SocialLink)`
-	color: black;
-`;
-
-// Styled components for Footer
-const FooterContainer = styled.footer`
-	background-color: rgba(255, 255, 255, 0.9);
-	width: 100%;
-	padding: 2rem 1rem;
-	box-sizing: border-box;
+const SocialContainer = styled.div`
 	display: flex;
 	justify-content: center;
-	align-items: flex-start;
-	flex-wrap: wrap;
-	// border-radius: 20px;
-
-	@media (max-width: 768px) {
-		justify-content: center;
-	}
-`;
-
-const Column = styled.div`
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	margin-bottom: 1rem;
-	padding: 0 1rem;
-
-	@media (max-width: 768px) {
-		width: 50%;
-	}
-`;
-
-const Header = styled.h3`
-	margin-bottom: 1rem;
-	font-size: 1.05rem;
-	text-align: center;
+	padding-bottom: 1rem;
 `;
 
 const FooterLink = styled(Link)`
@@ -101,6 +51,31 @@ const FooterLink = styled(Link)`
 	}
 `;
 
+const FooterContainer = styled.div`
+	display: flex;
+	justify-content: center;
+	width: 100%;
+	padding: 1rem 0;
+
+	@media (max-width: 768px) {
+		flex-direction: column;
+		align-items: center;
+	}
+`;
+
+const Column = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	margin: 0px 15px;
+`;
+
+const Header = styled.h3`
+	margin-bottom: 1rem;
+	font-size: 1.05rem;
+	text-align: center;
+`;
+
 const Copyright = styled.div`
 	width: 100%;
 	text-align: center;
@@ -108,78 +83,90 @@ const Copyright = styled.div`
 	font-size: 0.9rem;
 `;
 
-// Combined Component
 const Footer: React.FC = () => {
 	const currentYear = new Date().getFullYear();
+
+	const socialLinks = [
+		{
+			href: "https://www.youtube.com/channel/UCIV08RQSLOnOCFMe7Kj5-iA",
+			icon: faYoutube,
+			color: "#eb3223",
+		},
+		{
+			href: "https://www.facebook.com/profile.php?id=61552256902083",
+			icon: faFacebook,
+			color: "#4968ad",
+		},
+		{
+			href: "https://www.instagram.com/dollarsnlife/",
+			icon: faInstagram,
+			color: "black",
+		},
+	];
+
+	const footerLinks = [
+		{
+			header: "Discover",
+			links: [
+				{ to: "/calculator", text: "Calculators" },
+				{ to: "/budget-apps", text: "Budget Apps" },
+				{ to: "/investment", text: "Investment" },
+				{ to: "/passive-income-apps", text: "Passive Income Apps" },
+			],
+		},
+		{
+			header: "Earning",
+			links: [
+				{ to: "/passive-income", text: "Passive Income" },
+				{ to: "/side-hustles", text: "Side Hustles" },
+				{
+					to: "/category/deals-and-saving/manageyourfinances",
+					text: "Deals & Saving",
+				},
+				{ to: "/start-a-blog", text: "Start A Blog" },
+			],
+		},
+		{
+			header: "Who Are We?",
+			links: [
+				{ to: "/about-us", text: "About Us" },
+				{ to: "/contact-us", text: "Contact Us" },
+				{ to: "/our-team", text: "Our Team Members" },
+			],
+		},
+	];
+
 	return (
-		<>
+		<Container>
 			<SocialContainer>
-				<Youtube
-					href='https://www.youtube.com/channel/UCIV08RQSLOnOCFMe7Kj5-iA'
-					className='youtube social'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					<FontAwesomeIcon
-						icon={faYoutube as IconDefinition}
-						style={{ fontSize: "30px" }}
-					/>
-				</Youtube>
-				<Facebook
-					href='https://www.facebook.com/profile.php?id=61552256902083'
-					className='youtube social'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					<FontAwesomeIcon
-						icon={faFacebook as IconDefinition}
-						style={{ fontSize: "30px" }}
-					/>
-				</Facebook>
-				<Instagram
-					href='https://www.instagram.com/dollarsnlife/'
-					className='youtube social'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					<FontAwesomeIcon
-						icon={faInstagram as IconDefinition}
-						style={{ fontSize: "30px" }}
-					/>
-				</Instagram>
+				{socialLinks.map((link, index) => (
+					<SocialLink
+						key={index}
+						href={link.href}
+						target='_blank'
+						rel='noopener noreferrer'
+						style={{ color: link.color }}
+					>
+						<FontAwesomeIcon icon={link.icon} style={{ fontSize: "30px" }} />
+					</SocialLink>
+				))}
 			</SocialContainer>
 			<FooterContainer>
-				<Column>
-					<Header>Discover</Header>
-					<FooterLink to='/calculator'>Calculators</FooterLink>
-					<FooterLink to='/budget-apps'>Budget Apps</FooterLink>
-					<FooterLink to='/investment'>Investment</FooterLink>
-					<FooterLink to='/passive-income-apps'>Passive Income Apps</FooterLink>
-				</Column>
-				<Column>
-					<Header>Earning</Header>
-					<FooterLink to='/'>Home</FooterLink>
-					<FooterLink to='/category/manageyourfinances'>
-						Manage Your Finances
-					</FooterLink>
-					<FooterLink to='/passive-income'>Passive Income</FooterLink>
-					<FooterLink to='/side-hustles'>Side Hustles</FooterLink>
-					<FooterLink to='/category/deals-and-saving/manageyourfinances'>
-						Deals & Saving
-					</FooterLink>
-					<FooterLink to='/start-a-blog'>Start A Blog</FooterLink>
-				</Column>
-				<Column>
-					<Header>Who Are We?</Header>
-					<FooterLink to='/about-us'>About Us</FooterLink>
-					<FooterLink to='/contact-us'>Contact Us</FooterLink>
-					<FooterLink to='/our-team'>Our Team Members</FooterLink>
-				</Column>
-				<Copyright>
-					© {currentYear} All Rights Reserved Texas Connect LLC
-				</Copyright>
+				{footerLinks.map((column, index) => (
+					<Column key={index}>
+						<Header>{column.header}</Header>
+						{column.links.map((link, linkIndex) => (
+							<FooterLink key={linkIndex} to={link.to}>
+								{link.text}
+							</FooterLink>
+						))}
+					</Column>
+				))}
 			</FooterContainer>
-		</>
+			<Copyright>
+				© {currentYear} All Rights Reserved Texas Connect LLC
+			</Copyright>
+		</Container>
 	);
 };
 
