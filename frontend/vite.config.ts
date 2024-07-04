9556f339298471271db74adcb2189f244cea7c82
xxTarekxx <tarektawfik90@outlook.com>
2024-07-04 12:16:47 -0500

    restarting

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:8000',
-    },
-  },
-  build: {
-    outDir: 'dist',
-  },
+      '/api': 'http://localhost:8000'
+    }
+  }
 })
