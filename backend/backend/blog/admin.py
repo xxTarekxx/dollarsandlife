d3f68442b930c9bf3ccb5570b1f16b9568e7eb52
xxTarekxx <tarektawfik90@outlook.com>
2024-07-04 12:05:50 -0500

    restarting django structure bug cant be found

--- backend/backend/blog/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import BudgetPost
-
-@admin.register(BudgetPost)
-class BudgetPostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'datePosted')
-    search_fields = ('title', 'content', 'author')
--- backend/backend/blog/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import budget_posts
-
-urlpatterns = [
-    path('budget-posts/', budget_posts, name='budget_posts'),
-]
