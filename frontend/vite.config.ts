1cbf0e299722a74087d4f09d41016ffafa7c738c
xxTarekxx <tarektawfik90@outlook.com>
2024-07-06 14:25:15 -0500

    testing added

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Ensure this matches the root of your deployment
+  build: {
+    outDir: 'build', // Ensure this matches the build output directory
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-router-dom', 'react-dom'],
+        },
+      },
+    },
+  },
   server: {
-    proxy: {
-      '/api': 'http://localhost:8000'
-    }
-  }
-})
+    port: 3000,
+  },
+  preview: {
+    port: 4173,
+  },
+});
