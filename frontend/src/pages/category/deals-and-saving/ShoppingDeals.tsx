606f0adb9be8f0bf9c8d407f23d10c8e4f3f3f7e
xxtarekxx <tarektawfik90@outlook.com>
2025-02-26 21:58:13 -0600

    amazon id update

--- frontend/src/pages/category/deals-and-saving/ShoppingDeals.tsx
+++ frontend/src/pages/category/deals-and-saving/ShoppingDeals.tsx
@@ -116,7 +116,7 @@ const ShoppingDeals: React.FC = () => {
 	return (
 		<div className='PageContainer' ref={pageRef}>
 			<a
-				href='https://www.amazon.com/amazonprime?primeCampaignId=studentWlpPrimeRedir&linkCode=ll2&tag=dollarsandl0c-20&linkId=879184c8c8106f03c9fbbea8df411e86&language=en_US&ref_=as_li_ss_tl'
+				href='https://www.amazon.com/amazonprime?primeCampaignId=studentWlpPrimeRedir&linkCode=ll2&tag=dollarsandl02-20&linkId=879184c8c8106f03c9fbbea8df411e86&language=en_US&ref_=as_li_ss_tl'
 				target='_blank'
 				rel='noopener noreferrer'
 				className='TopBanner'
