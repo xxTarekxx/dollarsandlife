da227934ffde33460de5c7544b8a706c062e96cb
xxtarekxx <tarektawfik90@outlook.com>
2025-02-26 21:56:14 -0600

    breakingnews comp and App update

---
+++ frontend/src/pages/category/breakingnews/BreakingNews.tsx
@@ -0,0 +1,12 @@
+// src/pages/BreakingNews.tsx
+import React from "react";
+
+const BreakingNews: React.FC = () => {
+	return (
+		<div>
+			<h1>Breaking News</h1>
+		</div>
+	);
+};
+
+export default BreakingNews;
