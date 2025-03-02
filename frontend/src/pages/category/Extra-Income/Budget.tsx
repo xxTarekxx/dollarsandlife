ec8f3d5080b6f51ee64ee467a8e95fd7085109e9
xxtarekxx <tarektawfik90@outlook.com>
2025-03-01 22:48:38 -0600

    advertiser change

--- frontend/src/pages/category/Extra-Income/Budget.tsx
+++ frontend/src/pages/category/Extra-Income/Budget.tsx
@@ -7,7 +7,6 @@ import "../../../components/BlogPostContent.css";
 import PaginationContainer from "../../../components/PaginationContainer";
 import "./CommonStyles.css";
 
-// Defining a BlogPost type for better type safety
 interface BlogPost {
 	id: string;
 	title: string;
@@ -24,7 +23,6 @@ const Budget: React.FC = () => {
 	const pageRef = useRef<HTMLDivElement>(null);
 	const location = useLocation();
 
-	// Fetch the blog post data
 	useEffect(() => {
 		const fetchData = async () => {
 			try {
@@ -40,14 +38,12 @@ const Budget: React.FC = () => {
 		fetchData();
 	}, []);
 
-	// Scroll to the top when the currentPage changes
 	useEffect(() => {
 		if (pageRef.current) {
 			pageRef.current.scrollIntoView({ behavior: "smooth" });
 		}
 	}, [currentPage]);
 
-	// Update the document title based on the selected post
 	useEffect(() => {
 		const updateTitle = () => {
 			const pathSegments = location.pathname.split("/");
@@ -66,59 +62,16 @@ const Budget: React.FC = () => {
 		updateTitle();
 	}, [budgetPosts, location.pathname]);
 
-	// Extract an excerpt from the first content section
 	const getExcerpt = (content: { text: string }[]): string => {
 		const firstSection = content[0];
 		let excerpt = firstSection?.text || "";
-
-		if (excerpt.length > 200) {
-			excerpt = `${excerpt.substring(0, 200)}...`;
-		}
-
-		return excerpt;
+		return excerpt.length > 200 ? `${excerpt.substring(0, 200)}...` : excerpt;
 	};
 
-	// Paginate the blog posts
 	const currentPosts = budgetPosts.slice(
 		(currentPage - 1) * postsPerPage,
 		currentPage * postsPerPage,
 	);
-
-	const items = currentPosts.map((post, i) => (
-		<React.Fragment key={post.id}>
-			<div className='row-container'>
-				<Link to={`/extra-income/budget/${post.id}`}>
-					<BlogPostCard
-						id={post.id}
-						title={post.title}
-						imageUrl={post.imageUrl}
-						content={getExcerpt(post.content)}
-						author={post.author}
-						datePosted={post.datePosted}
-					/>
-				</Link>
-			</div>
-			{/* Show small ad (300x250) after every 2 rows */}
-			{i > 0 && i % 2 === 1 && (
-				<div className='postings-container'>
-					<a
-						href='https://www.kqzyfj.com/click-101252893-15236454'
-						target='_blank'
-						rel='noopener noreferrer'
-					>
-						<img
-							srcSet='https://www.ftjcfx.com/image-101252893-15236454 1x, https://www.ftjcfx.com/image-101252893-15236454@2x.jpg 2x'
-							width='300'
-							height='250'
-							alt='Small Ad'
-							className='postings-image'
-							loading='lazy'
-						/>
-					</a>
-				</div>
-			)}
-		</React.Fragment>
-	));
 
 	return (
 		<div className='page-container' ref={pageRef}>
@@ -138,37 +91,60 @@ const Budget: React.FC = () => {
 										src='/images/shoppinganddeals/amazon-banner.webp'
 										alt='Amazon Prime Banner'
 										className='TopBannerImage'
-										loading='eager'
-										srcSet='/images/shoppinganddeals/amazon-banner.webp 1x, /images/shoppinganddeals/amazon-banner@2x.webp 2x'
 									/>
 									<button className='topbanner-button'>Free Trial</button>
 								</a>
 							</div>
 							<h1>Budget Guides</h1>
-							<div className='content-wrapper'>{items}</div>
+							<div className='content-wrapper'>
+								{currentPosts.map((post, i) => (
+									<React.Fragment key={post.id}>
+										<div className='row-container'>
+											<Link to={`/extra-income/budget/${post.id}`}>
+												<BlogPostCard
+													id={post.id}
+													title={post.title}
+													imageUrl={post.imageUrl}
+													content={getExcerpt(post.content)}
+													author={post.author}
+													datePosted={post.datePosted}
+												/>
+											</Link>
+										</div>
+										{i > 0 && i % 2 === 1 && (
+											<div className='postings-container'>
+												<ins
+													className='adsbygoogle'
+													style={{ display: "block" }}
+													data-ad-client='ca-pub-2295073683044412'
+													data-ad-slot='YOUR_AD_SLOT'
+													data-ad-format='auto'
+													data-full-width-responsive='true'
+												></ins>
+											</div>
+										)}
+									</React.Fragment>
+								))}
+							</div>
 							<PaginationContainer
 								totalItems={budgetPosts.length}
 								itemsPerPage={postsPerPage}
 								currentPage={currentPage}
 								setCurrentPage={setCurrentPage}
 							/>
-							{/* Show large ad (728x90) at the very bottom */}
 							<div className='postings-container'>
-								<div className='postings-bottom-container'>
-									<a
-										href='https://www.tkqlhce.com/click-101252893-14103279'
-										target='_blank'
-										rel='noopener noreferrer'
-									>
-										<img
-											className='postings-image'
-											srcSet='https://www.ftjcfx.com/image-101252893-14103279 1x, https://www.ftjcfx.com/image-101252893-14103279@2x.jpg 2x'
-											alt='Speak a new language fluently fast. Start now!'
-											loading='lazy'
-										/>
-									</a>
-								</div>
+								<ins
+									className='adsbygoogle'
+									style={{ display: "block" }}
+									data-ad-client='ca-pub-2295073683044412'
+									data-ad-slot='YOUR_BOTTOM_AD_SLOT'
+									data-ad-format='auto'
+									data-full-width-responsive='true'
+								></ins>
 							</div>
+							<script>
+								{`(adsbygoogle = window.adsbygoogle || []).push({});`}
+							</script>
 						</>
 					}
 				/>
