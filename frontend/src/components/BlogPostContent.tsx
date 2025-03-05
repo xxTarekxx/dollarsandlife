d08b42588d08afea9fe12dd49ebd4b06d98860b1
xxtarekxx <tarektawfik90@outlook.com>
2025-03-04 21:16:37 -0600

    ads id

--- frontend/src/components/BlogPostContent.tsx
+++ frontend/src/components/BlogPostContent.tsx
@@ -169,7 +169,7 @@ const BlogPostContent: React.FC<BlogPostContentProps> = ({ jsonFile }) => {
 									minWidth: "300px",
 									minHeight: "250px",
 								}}
-								data-ad-client='ca-pub-2295073683044412'
+								data-ad-client='ca-pub-1079721341426198'
 								data-ad-slot='9380614635'
 								data-ad-format='rectangle'
 								data-full-width-responsive='false'
@@ -189,7 +189,7 @@ const BlogPostContent: React.FC<BlogPostContentProps> = ({ jsonFile }) => {
 						minWidth: "728px",
 						minHeight: "90px",
 					}}
-					data-ad-client='ca-pub-2295073683044412'
+					data-ad-client='ca-pub-1079721341426198'
 					data-ad-slot='9380614635'
 					data-ad-format='horizontal'
 					data-full-width-responsive='false'
