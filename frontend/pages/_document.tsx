2630d66069904bc12bcb9c58baef6c1d128ae03a
Tarek <tarektawfik90@outlook.com>
2025-08-13 20:59:28 -0500

    google

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document, { DocumentContext, Html, Head, Main, NextScript } from 'next/document';
+import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
@@ -32,6 +32,18 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en"> {/* Optional: Add a lang attribute */}
         <Head>
+          {/* Google tag (gtag.js) */}
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-S7FWNHSD7P"></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-S7FWNHSD7P');
+              `,
+            }}
+          />
           {/* Favicon links and other static head elements can go here if not in _app.js Head */}
           {/* For example, if you have specific font links that must be in _document */}
         </Head>
